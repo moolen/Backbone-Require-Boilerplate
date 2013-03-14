d03bbd695495f842f277030b9952eae31a75e785
moolen <beller.moritz@googlemail.com>
2013-03-14 13:40:49 +0100

    using bower for frontend package management

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,16 @@ require.config({
   paths: {
 
       // Core Libraries
-      "jquery": "./libs/jquery",
-      "underscore": "./libs/lodash",
-      "backbone": "./libs/backbone",
-
+      "jquery": "./libs/jquery/jquery.min",
+      "underscore": "./libs/lodash/lodash",
+      "backbone": "./libs/backbone/backbone-min",
+      "jqueryui": "./libs/jquery-ui/ui/jquery-ui.custom",
+      
       // Plugins
       "backbone.validateAll": "./libs/plugins/Backbone.validateAll",
       "text": "./libs/plugins/text",
-      "datepicker" : "./libs/datepicker",
-      "jqueryui": "./libs/jqueryui",
+      "datepicker" : "./libs/datepicker/js/bootstrap-datepicker",
+      
 
       // Application Folders
       "collections": "./app/collections",
